678ab2aee87c65350351404aacf1040d814d9a13
liuchang <liuchang@yimian.com.cn>
2022-01-31 13:23:17 +0800

    加入增产剂功能

--- all_test.go
+++ all_test.go
@@ -6,131 +6,147 @@ import (
 
 var StartContext = &ManufactureParameters{
 	FacilityRate: map[string]float64{
-		"制造台": 0.75,
+		"制造台":  0.75,
 		"电弧熔炉": 1,
 	},
-	Formula: map[string]int{
+	FormulaNums: map[string]int{
 		"氢": 1,
 	},
 }
 
 var SecondContext = &ManufactureParameters{
 	FacilityRate: map[string]float64{
-		"制造台": 0.75,
+		"制造台":  0.75,
 		"电弧熔炉": 1,
 	},
-	Formula: map[string]int{
-		"氢": 1,
-		"石墨烯": 1,
+	FormulaNums: map[string]int{
+		"氢":    1,
+		"石墨烯":  1,
 		"有机晶体": 1,
 	},
 }
 
-var DefaultContext = &ManufactureParameters{
+var BlackboxContext = &ManufactureParameters{
 	FacilityRate: map[string]float64{
-		"制造台": 1.5,
+		"制造台":  1.5,
 		"电弧熔炉": 2,
 	},
-	Formula: map[string]int{
-		"石墨烯": 1,
-		"有机晶体": 1,
-		"晶格硅": 1,
-		"金刚石": 1,
-		"碳纳米管": 1,
-		"卡西米尔晶体": 0,
-		"粒子容器": 0,
-		"光子合并器": 0,
-		"氢": 1,
-		"重氢": 1,
-	},
-}
-
-var AllResContext = &ManufactureParameters{
-	FacilityRate: map[string]float64{
-		"制造台": 1.5,
-		"电弧熔炉": 2,
-	},
-	Formula: map[string]int{
-		"石墨烯": 1,
-		"有机晶体": 1,
-		"晶格硅": 1,
-		"金刚石": 1,
-		"碳纳米管": 1,
+	FormulaNums: map[string]int{
+		"硫酸":     1,
+		"石墨烯":    1,
+		"有机晶体":   1,
+		"晶格硅":    1,
+		"金刚石":    1,
+		"碳纳米管":   1,
 		"卡西米尔晶体": 1,
-		"粒子容器": 1,
-		"光子合并器": 1,
-		"氢": 1,
-		"重氢": 1,
+		"粒子容器":   1,
+		"光子合并器":  1,
+		"氢":      1,
+		"重氢":     1,
 	},
+	SprayingResources: map[string]SprayType{
+		"增产剂MK1": SprayDisabled,
+		"增产剂MK2": SprayDisabled,
+		"增产剂MK3": SprayDisabled,
+	},
+	ImportingResources: map[string]bool{
+		"粒子容器": true,
+		"铁矿": true,
+		"铜矿": true,
+		"石矿": true,
+		"煤矿": true,
+		"钛矿": true,
+		"硅石": true,
+		"水": true,
+		"氢": true,
+		"重氢": true,
+		"原油": true,
+		"可燃冰": true,
+		"有机晶体": true,
+		"刺笋结晶": true,
+		"光栅石": true,
+		"金伯利矿石": true,
+		"分形硅": true,
+		"临界光子": true,
+	},
+	SprayingClass: SprayingMark3,
 }
 
-func TestStart(t *testing.T) {
-	p := &ManufactureParameters{
-		FacilityRate: map[string]float64{
-			"制造台": 0.75,
-			"电弧熔炉": 1,
-		},
-		Formula: map[string]int{
-			"氢": 1,
-		},
-		ImportingResources: map[string]bool{
-			"氢": true,
-		},
-	}
+func TestMatrix(t *testing.T) {
+	c := BlackboxContext.Clone()
+	//c.SprayingResources["磁线圈"] = SpraySpeedUp
+	//c.SprayingResources["电路板"] = SpraySpeedUp
+	c.SprayingResources["卡西米尔晶体"] = SpraySpeedUp
+	c.SprayingResources["钛晶石"] = SpraySpeedUp
+	c.SprayingResources["奇异物质"] = SpraySpeedUp
+	c.SprayingResources["能量矩阵"] = SpraySpeedUp
+	c.SprayingResources["晶格硅"] = SpraySpeedUp
 
-	p.ShowRequirement([]ResourceRate{
-		{"硫酸", 6.0},
-	})
-	p.ImportingResources["硫酸"] = true
-
-	p.ShowRequirement([]ResourceRate{
-		{"处理器", 3.0},
-	})
-	p.ImportingResources["处理器"] = true
-
-
-	p.ShowRequirement([]ResourceRate{
-		{"粒子容器", 0.375},
-	})
-	p.ImportingResources["粒子容器"] = true
-
-	p.ShowRequirement([]ResourceRate{
-		{"电磁涡轮", 0.375},
-	})
-	p.ImportingResources["电磁涡轮"] = true
-
-	p.ShowRequirement([]ResourceRate{
-		{"钛块", 1.0},
-		{"钛合金", 3.0},
-	})
-	p.ImportingResources["钛块"] = true
-	p.ImportingResources["钛合金"] = true
-
-	p.ShowRequirement([]ResourceRate{
-		{"行星内物流运输站", 1.0/60},
-		{"星际物流运输站", 0.5/60},
-		{"物流运输机", 22.5/60},
-		{"星际物流运输船", 7.5/60},
-	})
+	c.ShowRequirement(map[string]float64{
+		"宇宙矩阵": 10,
+	}, templateMatrix)
 }
 
-func TestRocketBox(t *testing.T) {
+const templateMatrix = `
+粒子容器
 
-	(&ManufactureParameters{
-		FacilityRate: map[string]float64{
-			"制造台": 1.5,
-			"电弧熔炉": 2,
-		},
-		Formula: map[string]int{
-			"石墨烯": 1,
-			"有机晶体": 1,
-			"晶格硅": 1,
-			"金刚石": 1,
-			"碳纳米管": 1,
-			"氢": 1,
-			"重氢": 1,
-		},
-	}).ShowRequirement([]ResourceRate{
-		{"小型运输火箭",0.4},
-	})
-}
+铁矿
+铜矿
+石矿
+煤矿
+钛矿
+硅石
+水
+氢
+重氢
+原油
+可燃冰
+有机晶体
+刺笋结晶
+光栅石
+金伯利矿石
+分形硅
+临界光子
+
+增产剂MK1
+增产剂MK2
+增产剂MK3
+
+铁块
+铜块
+电路板
+磁铁
+磁线圈
+电磁矩阵
+
+高能石墨
+能量矩阵
+
+金刚石
+钛块
+钛晶石
+结构矩阵
+
+高纯硅块
+精炼油
+塑料
+碳纳米管
+晶格硅
+粒子宽带
+微晶元件
+处理器
+信息矩阵
+
+石墨烯
+卡西米尔晶体
+玻璃
+钛化玻璃
+位面过滤器
+量子芯片
+奇异物质
+引力透镜
+引力矩阵
+
+反物质
+宇宙矩阵
+`
--- resources.go
+++ resources.go
@@ -3,397 +3,90 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
-type Resource struct {
-	Name         string
-	Formula      int     //配方编号
-	Facility     string
+type SprayType int
+
+var (
+	SprayDisabled = SprayType(0)
+	SpraySpeedUp  = SprayType(1)
+	SprayBonus    = SprayType(2)
+)
+
+type SprayingClass struct {
+	Resource    string
+	SpeedUpRate float64
+	BonusRate   float64
+	PowerRate   float64
+	Count       float64
+}
+
+type Formula struct {
+	ResourceName string  //目标资源名称
+	FormulaNum   int     //配方编号
+	Facility     string  //生产设施
 	Duration     float64 //生产时长
-	Num          float64 //每次产出数量
-	Requirements []ResourceRequirement
+	OutputNum    float64 //每次产出数量
+	CanSpeedUp   bool    //支持增产剂加速生产
+	CanBonus     bool    //支持增产剂额外产出
+	Materials    []ResourceNum
 }
 
-func (r *Resource) Key() string {
-	return fmt.Sprintf("%s-%d", r.Name, r.Formula)
+func (f *Formula) Key() string {
+	return fmt.Sprintf("%s-%d", f.ResourceName, f.FormulaNum)
 }
 
-type ResourceRequirement struct {
+type Produce struct {
+	*Formula
+	OutputRes    string
+	OutputRate   float64
+	SprayType    SprayType
+	SprayRate    float64
+	Inputs       map[string]float64
+	FacilityName string
+	FacilityNum  float64
+}
+
+type ResourceNum struct {
 	ResourceName string
 	Num          float64 //每次需求数量
 }
 
 type ManufactureParameters struct {
 	FacilityRate       map[string]float64
-	Formula            map[string]int
+	FormulaNums        map[string]int
 	ImportingResources map[string]bool
+	SprayingResources  map[string]SprayType
+	SprayingClass      *SprayingClass
 }
 
-var AllResources = []*Resource{
-	//基础资源
-	{"铁矿", 0, "采矿机", 1, 1, nil},
-	{"铜矿", 0, "采矿机", 1, 1, nil},
-	{"石矿", 0, "采矿机", 1, 1, nil},
-	{"煤矿", 0, "采矿机", 1, 1, nil},
-	{"钛矿", 0, "采矿机", 1, 1, nil},
-	{"硅石", 0, "采矿机", 1, 1, nil},
-	{"水", 0, "抽水机", 1, 1, nil},
-	{"氢", 0, "原油精炼厂", 8, 3, []ResourceRequirement{
-		{"原油", 2},
-		{"高能石墨", -2},
-	}},
-	{"氢", 1, "行星采集器", 1, 20, nil},
-	{"重氢", 0, "分馏器", 100.0 / 30, 1, []ResourceRequirement{
-		{"氢", 1},
-	}},
-	{"重氢", 1, "行星采集器", 1, 1, nil},
-
-	//珍奇
-	{"原油", 0, "原油萃取机", 1, 1, nil},
-	{"硫酸",1,  "抽水机", 1, 1, nil},
-	{"可燃冰", 0, "行星采集器", 1, 1, nil},
-	{"有机晶体",1,  "采矿机", 1, 1, nil},
-	{"刺笋结晶", 0, "采矿机", 1, 1, nil},
-	{"光栅石",0,  "采矿机", 1, 1, nil},
-	{"金伯利矿石", 0, "采矿机", 1, 1, nil},
-	{"分形硅",0,  "采矿机", 1, 1, nil},
-	{"单极磁石",0,  "采矿机", 1, 1, nil},
-
-	//
-	{"临界光子", 0, "微波接收器", 12, 1, nil},
-	{"反物质", 0, "粒子对撞机", 2, 2, []ResourceRequirement{
-		{"临界光子", 2},
-		{"氢", -2},
-	}},
-
-	//
-	{"铁块", 0, "电弧熔炉", 1, 1, []ResourceRequirement{
-		{"铁矿", 1},
-	}},
-	{"磁铁", 0, "电弧熔炉", 1.5, 1, []ResourceRequirement{
-		{"铁矿", 1},
-	}},
-	{"铜块", 0, "电弧熔炉", 1, 1, []ResourceRequirement{
-		{"铜矿", 1},
-	}},
-	{"高纯硅块", 0, "电弧熔炉", 2, 1, []ResourceRequirement{
-		{"硅石", 2},
-	}},
-	{"钛块", 0, "电弧熔炉", 2, 1, []ResourceRequirement{
-		{"钛矿", 2},
-	}},
-	{"石材", 0, "电弧熔炉", 1, 1, []ResourceRequirement{
-		{"石矿", 1},
-	}},
-	{"玻璃",0,  "电弧熔炉", 2, 1, []ResourceRequirement{
-		{"石矿", 2},
-	}},
-	{"高能石墨", 0, "电弧熔炉", 2, 1, []ResourceRequirement{
-		{"煤矿", 2},
-	}},
-	{"金刚石", 0, "电弧熔炉", 2, 1, []ResourceRequirement{
-		{"高能石墨", 1},
-	}},
-	{"金刚石",1,  "电弧熔炉", 1.5, 2, []ResourceRequirement{
-		{"金伯利矿石", 1},
-	}},
-	{"精炼油",0,  "原油精炼厂", 4, 2, []ResourceRequirement{
-		{"原油", 2},
-		{"氢", -1},
-	}},
-	{"晶格硅", 0, "电弧熔炉", 2, 1, []ResourceRequirement{
-		{"高纯硅块", 1},
-	}},
-	{"晶格硅", 1, "制造台", 1.5, 2, []ResourceRequirement{
-		{"分形硅", 1},
-	}},
-	{"钢材",0,  "电弧熔炉", 3, 1, []ResourceRequirement{
-		{"铁块", 3},
-	}},
-
-	{"硫酸",0,  "化工厂", 6, 4, []ResourceRequirement{
-		{"精炼油", 6},
-		{"石矿", 8},
-		{"水", 4},
-	}},
-	{"塑料",0,  "化工厂", 3, 1, []ResourceRequirement{
-		{"精炼油", 2},
-		{"高能石墨", 1},
-	}},
-	{"有机晶体",0,  "化工厂", 6, 1, []ResourceRequirement{
-		{"塑料", 2},
-		{"精炼油", 1},
-		{"水", 1},
-	}},
-	{"石墨烯", 0, "化工厂", 3, 2, []ResourceRequirement{
-		{"高能石墨", 3},
-		{"硫酸", 1},
-	}},
-	{"石墨烯", 1, "化工厂", 2, 2, []ResourceRequirement{
-		{"可燃冰", 2},
-		{"氢", -1},
-	}},
-	{"碳纳米管", 0, "化工厂", 4, 2, []ResourceRequirement{
-		{"石墨烯", 3},
-		{"钛块", 1},
-	}},
-	{"碳纳米管", 1, "化工厂", 4, 2, []ResourceRequirement{
-		{"刺笋结晶", 2},
-	}},
-
-
-
-	{"电路板",0,  "制造台", 1, 2, []ResourceRequirement{
-		{"铁块", 2},
-		{"铜块", 1},
-	}},
-	{"磁线圈", 0, "制造台", 1, 2, []ResourceRequirement{
-		{"磁铁", 2},
-		{"铜块", 1},
-	}},
-	{"齿轮", 0, "制造台", 1, 1, []ResourceRequirement{
-		{"铁块", 1},
-	}},
-	{"电动机",0,  "制造台", 2, 1, []ResourceRequirement{
-		{"铁块", 2},
-		{"齿轮", 1},
-		{"磁线圈", 1},
-	}},
-	{"电磁涡轮", 0, "制造台", 2, 1, []ResourceRequirement{
-		{"电动机", 2},
-		{"磁线圈", 2},
-	}},
-	{"超级磁场环", 0, "制造台", 3, 1, []ResourceRequirement{
-		{"电磁涡轮", 2},
-		{"磁铁", 3},
-		{"高能石墨", 1},
-	}},
-	{"粒子容器", 0, "制造台", 4, 1, []ResourceRequirement{
-		{"电磁涡轮", 2},
-		{"铜块", 2},
-		{"石墨烯", 2},
-	}},
-	{"粒子容器", 1, "制造台", 4, 1, []ResourceRequirement{
-		{"单极磁石", 10},
-		{"铜块", 2},
-	}},
-
-	//==============================
-
-	{"棱镜",0,  "制造台", 2, 2, []ResourceRequirement{
-		{"玻璃", 3},
-	}},
-	{"电浆激发器", 0, "制造台", 2, 1, []ResourceRequirement{
-		{"磁线圈", 4},
-		{"棱镜", 2},
-	}},
-	{"钛晶石",0,  "制造台", 4, 1, []ResourceRequirement{
-		{"有机晶体", 1},
-		{"钛块", 3},
-	}},
-	{"钛合金",0,  "电弧熔炉", 12, 4, []ResourceRequirement{
-		{"钛块", 4},
-		{"钢材", 4},
-		{"硫酸", 8},
-	}},
-
-	//==============================
-
-	{"粒子宽带", 0, "制造台", 8, 1, []ResourceRequirement{
-		{"碳纳米管", 2},
-		{"晶格硅", 2},
-		{"塑料", 1},
-	}},
-	{"微晶元件",0,  "制造台", 2, 1, []ResourceRequirement{
-		{"高纯硅块", 2},
-		{"铜块", 1},
-	}},
-	{"处理器", 0, "制造台", 3, 1, []ResourceRequirement{
-		{"电路板", 2},
-		{"微晶元件", 2},
-	}},
-
-	//==============================
-
-	{"钛化玻璃",0,  "制造台", 5, 2, []ResourceRequirement{
-		{"玻璃", 2},
-		{"钛块", 2},
-		{"水", 2},
-	}},
-	{"卡西米尔晶体", 0, "制造台", 4, 1, []ResourceRequirement{
-		{"钛晶石", 1},
-		{"石墨烯", 2},
-		{"氢", 12},
-	}},
-	{"卡西米尔晶体", 1, "制造台", 4, 1, []ResourceRequirement{
-		{"光栅石", 4},
-		{"石墨烯", 2},
-		{"氢", 12},
-	}},
-	{"位面过滤器",0,  "制造台", 12, 1, []ResourceRequirement{
-		{"卡西米尔晶体", 1},
-		{"钛化玻璃", 2},
-	}},
-	{"量子芯片", 0, "制造台", 6, 1, []ResourceRequirement{
-		{"处理器", 2},
-		{"位面过滤器", 2},
-	}},
-
-	{"奇异物质",0,  "粒子对撞机", 8, 1, []ResourceRequirement{
-		{"粒子容器", 2},
-		{"铁块", 2},
-		{"重氢", 10},
-	}},
-
-	{"引力透镜",0,  "制造台", 6, 1, []ResourceRequirement{
-		{"金刚石", 4},
-		{"奇异物质", 1},
-	}},
-
-	//==============================
-
-	{"液氢燃料棒",0,  "制造台", 6, 2, []ResourceRequirement{
-		{"钛块", 1},
-		{"氢", 10},
-	}},
-	{"氘核燃料棒",0,  "制造台", 12, 2, []ResourceRequirement{
-		{"重氢", 20},
-		{"钛合金", 1},
-		{"超级磁场环", 1},
-	}},
-	{"湮灭约束球",0,  "制造台", 20, 1, []ResourceRequirement{
-		{"粒子容器", 1},
-		{"处理器", 1},
-	}},
-	{"反物质燃料棒",0,  "制造台", 24, 2, []ResourceRequirement{
-		{"反物质", 12},
-		{"氢", 12},
-		{"湮灭约束球", 1},
-		{"钛合金", 1},
-	}},
-
-	//==============================
-
-	{"推进器",0,  "制造台", 4, 1, []ResourceRequirement{
-		{"钢材", 2},
-		{"铜块", 3},
-	}},
-	{"物流运输机",0,  "制造台", 4, 1, []ResourceRequirement{
-		{"铁块", 5},
-		{"处理器", 2},
-		{"推进器", 2},
-	}},
-	{"加力推进器",0,  "制造台", 6, 1, []ResourceRequirement{
-		{"钛合金", 5},
-		{"电磁涡轮", 5},
-	}},
-	{"星际物流运输船", 0, "制造台", 6, 1, []ResourceRequirement{
-		{"钛合金", 10},
-		{"处理器", 10},
-		{"加力推进器", 2},
-	}},
-	{"空间翘曲器",0,  "制造台", 10, 8, []ResourceRequirement{
-		{"引力矩阵", 1},
-	}},
-
-	//==============================
-
-	{"光子合并器", 0, "制造台", 3, 1, []ResourceRequirement{
-		{"棱镜", 2},
-		{"电路板", 1},
-	}},
-	{"光子合并器",1,  "制造台", 3, 1, []ResourceRequirement{
-		{"光栅石", 1},
-		{"电路板", 1},
-	}},
-	{"太阳帆",0,  "制造台", 4, 2, []ResourceRequirement{
-		{"石墨烯", 1},
-		{"光子合并器", 1},
-	}},
-	{"框架材料", 0, "制造台", 6, 1, []ResourceRequirement{
-		{"碳纳米管", 4},
-		{"钛合金", 1},
-		{"高纯硅块", 1},
-	}},
-	{"戴森球组件", 0, "制造台", 8, 1, []ResourceRequirement{
-		{"框架材料", 3},
-		{"太阳帆", 3},
-		{"处理器", 3},
-	}},
-	{"小型运输火箭",0,  "制造台", 6, 1, []ResourceRequirement{
-		{"戴森球组件", 2},
-		{"氘核燃料棒", 4},
-		{"量子芯片", 2},
-	}},
-
-	//==============================
-
-	{"电磁矩阵", 0, "研究所", 3, 1, []ResourceRequirement{
-		{"磁线圈", 1},
-		{"电路板", 1},
-	}},
-	{"能量矩阵", 0, "研究所", 6, 1, []ResourceRequirement{
-		{"高能石墨", 2},
-		{"氢", 2},
-	}},
-	{"结构矩阵", 0, "研究所", 8, 1, []ResourceRequirement{
-		{"金刚石", 1},
-		{"钛晶石", 1},
-	}},
-	{"信息矩阵", 0, "研究所", 10, 1, []ResourceRequirement{
-		{"处理器", 2},
-		{"粒子宽带", 1},
-	}},
-	{"引力矩阵", 0, "研究所", 24, 2, []ResourceRequirement{
-		{"引力透镜", 1},
-		{"量子芯片", 1},
-	}},
-	{"宇宙矩阵", 0, "研究所", 15, 1, []ResourceRequirement{
-		{"电磁矩阵", 1},
-		{"能量矩阵", 1},
-		{"结构矩阵", 1},
-		{"信息矩阵", 1},
-		{"引力矩阵", 1},
-		{"反物质", 1},
-	}},
-
-	//==============================
-
-	{"行星内物流运输站", 0, "制造台", 20, 1, []ResourceRequirement{
-		{"钢材", 40},
-		{"钛块", 40},
-		{"处理器", 40},
-		{"粒子容器", 20},
-	}},
-	{"星际物流运输站", 0, "制造台", 30, 1, []ResourceRequirement{
-		{"行星内物流运输站", 1},
-		{"钛合金", 40},
-		{"粒子容器", 20},
-	}},
-	{"推进器", 0, "制造台", 4, 1, []ResourceRequirement{
-		{"钢材", 2},
-		{"铜块", 3},
-	}},
-	{"物流运输机", 0, "制造台", 4, 1, []ResourceRequirement{
-		{"铁块", 5},
-		{"处理器", 2},
-		{"推进器", 2},
-	}},
-	{"加力推进器", 0, "制造台", 6, 1, []ResourceRequirement{
-		{"钛合金", 5},
-		{"电磁涡轮", 5},
-	}},
-	{"星际物流运输船", 0, "制造台", 6, 1, []ResourceRequirement{
-		{"钛合金", 10},
-		{"处理器", 10},
-		{"加力推进器", 2},
-	}},
-
+func (mp *ManufactureParameters) Clone() *ManufactureParameters {
+	r := &ManufactureParameters{
+		FacilityRate:       map[string]float64{},
+		FormulaNums:        map[string]int{},
+		ImportingResources: map[string]bool{},
+		SprayingResources:  map[string]SprayType{},
+	}
+	for k, v := range mp.FacilityRate {
+		r.FacilityRate[k] = v
+	}
+	for k, v := range mp.FormulaNums {
+		r.FormulaNums[k] = v
+	}
+	for k, v := range mp.ImportingResources {
+		r.ImportingResources[k] = v
+	}
+	for k, v := range mp.SprayingResources {
+		r.SprayingResources[k] = v
+	}
+	r.SprayingClass = mp.SprayingClass
+	return r
 }
 
-var mappingResources = func() map[string]*Resource {
-	m := map[string]*Resource{}
-	for _, r := range AllResources {
+var mappingResources = func() map[string]*Formula {
+	m := map[string]*Formula{}
+	for _, r := range AllFormulas {
 		m[r.Key()] = r
 	}
 	return m
@@ -401,151 +94,213 @@ var mappingResources = func() map[string]*Resource {
 
 var resourceOrder = func() map[string]int {
 	m := map[string]int{}
-	for i, r := range AllResources {
-	m[r.Key()] = i
+	for i, r := range AllFormulas {
+		m[r.Key()] = i
 	}
 	return m
 }()
 
-var facilityOrder = map[string]int{
-	"采矿机":    1,
-	"原油萃取机":  2,
-	"抽水机":    3,
-	"行星采集器":  4,
-	"电弧熔炉":   5,
-	"原油精炼厂":  6,
-	"化工厂":    7,
-	"分馏器":    8,
-	"制造台": 9,
-	"粒子对撞机":  10,
-	"微波接收器":  11,
-	"研究所": 12,
-}
-
-type ResourceRate struct {
-	ResourceName string
-	Rate         float64
-}
-
-func (ctx *ManufactureParameters) getResource(resName string) *Resource {
-	if ctx.ImportingResources[resName] {
-		return &Resource{
-			Name: resName,
-			Formula: -1,
-			Facility: "外部输入",
-			Duration: 1,
-			Num: 1,
+func (mp *ManufactureParameters) getFormula(resName string) *Formula {
+	if mp.ImportingResources[resName] {
+		return &Formula{
+			ResourceName: resName,
+			FormulaNum:   -1,
+			Facility:     "外部输入",
+			Duration:     1,
+			OutputNum:    1,
 		}
 	}
-	resKey := fmt.Sprintf("%s-%d", resName, ctx.Formula[resName])
+	resKey := fmt.Sprintf("%s-%d", resName, mp.FormulaNums[resName])
 	return mappingResources[resKey]
 }
 
-//递归计算所有资源公式
-//返回 资源名称 - 制造设备数量
-func (ctx *ManufactureParameters) calculateRequirement(resRate ResourceRate) map[string]float64 {
-	result := map[string]float64{}
+//迭代计算所有资源公式
+func (mp *ManufactureParameters) calculateProduce(requirements map[string]float64) map[string]*Produce {
+	producesMap := map[string]*Produce{}
+	for changed := true; changed; {
+		changed = false
+		for resName, resRate := range requirements {
+			if resRate < 0.000001 {
+				continue
+			}
+			f := mp.getFormula(resName)
 
-	res := ctx.getResource(resRate.ResourceName)
-	if res == nil {
-		panic(fmt.Errorf("配方 %s 不存在", resRate.ResourceName))
-	}
-	for _, resReq := range res.Requirements {
-		subResReq := ctx.calculateRequirement(ResourceRate{
-			ResourceName: resReq.ResourceName,
-			Rate:         resRate.Rate / res.Num * resReq.Num,
-		})
-		for subRes, subNum := range subResReq {
-			result[subRes] += subNum
+			//生产速度加成
+			rateRatio := 1.0
+			//产出加成
+			outputRatio := 1.0
+
+			//设施生产速度
+			facilityRatio, ok := mp.FacilityRate[f.Facility]
+			if !ok {
+				facilityRatio = 1.0
+			}
+
+			//计算喷涂加速
+			st := mp.getSprayType(f)
+			switch st {
+			case SprayDisabled:
+			case SpraySpeedUp:
+				rateRatio = mp.SprayingClass.SpeedUpRate
+			case SprayBonus:
+				outputRatio = mp.SprayingClass.BonusRate
+			}
+
+			//每秒生产（公式应用）次数
+			produceRate := resRate / f.OutputNum / outputRatio
+			//设施数量
+			facilityNum := produceRate * f.Duration / facilityRatio / rateRatio
+
+			materials := f.Materials
+			var sprayRate float64
+			if st != SprayDisabled {
+				//计算喷涂开销
+				materials = make([]ResourceNum, len(f.Materials), len(f.Materials)+1)
+				copy(materials, f.Materials)
+				sumSprayCount := 0.0
+				for _, m := range f.Materials {
+					sumSprayCount += m.Num
+				}
+				materials = append(materials, ResourceNum{
+					ResourceName: mp.SprayingClass.Resource,
+					Num:          sumSprayCount / mp.SprayingClass.Count,
+				})
+				sprayRate = sumSprayCount / mp.SprayingClass.Count
+			}
+
+			//先从需求中减去这个资源
+			delete(requirements, resName)
+
+			//创建 Produce 生产详情项
+			p, ok := producesMap[resName]
+			if !ok {
+				p = &Produce{
+					Formula:     f,
+					OutputRes:   resName,
+					SprayType:   st,
+					Inputs:      map[string]float64{},
+				}
+				producesMap[resName] = p
+			}
+			p.OutputRate += resRate
+			p.FacilityNum += facilityNum
+			p.SprayRate += sprayRate * produceRate
+			for _, m := range f.Materials {
+				p.Inputs[m.ResourceName] += m.Num * produceRate
+			}
+			for _, m := range materials {
+				requirements[m.ResourceName] += m.Num * produceRate
+			}
+			changed = true
 		}
 	}
-	facilityRate, rateExists := ctx.FacilityRate[res.Facility]
-	if !rateExists {
-		facilityRate = 1
-	}
-	result[resRate.ResourceName] += resRate.Rate * res.Duration / res.Num / facilityRate
-	return result
+	return producesMap
 }
 
-type FacilityRequirement struct {
-	Resource   string
-	Facility   string
-	Num        float64
-	Inputs     []ResourceRate
-	OutputRate float64
-}
-
-func (ctx *ManufactureParameters) FormatReq(fr *FacilityRequirement) string {
-	facilityRate, exists := ctx.FacilityRate[fr.Facility]
-	if !exists {
-		facilityRate = 1
+func (mp *ManufactureParameters) getSprayType(f *Formula) SprayType {
+	if mp.SprayingClass == nil {
+		return SprayDisabled
 	}
 
-	s := fmt.Sprintf("%s %.2f/s <- (%s:%.2f) <- (", fr.Resource, fr.OutputRate*facilityRate, fr.Facility, fr.Num)
-	for i, input := range fr.Inputs {
-		if i > 0 {
-			s += " "
+	t, ok := mp.SprayingResources[f.ResourceName]
+	if !ok {
+		if defaultIncreaseFacilities[f.Facility] {
+			t = SprayBonus
+		} else {
+			t = SpraySpeedUp
 		}
-		s += fmt.Sprintf("%s: %.2f/s", input.ResourceName, input.Rate*facilityRate)
 	}
-	s += ")"
-	return s
+	if !f.CanBonus && t == SprayBonus {
+		t = SpraySpeedUp
+	}
+	if !f.CanSpeedUp && t == SpraySpeedUp {
+		t = SprayDisabled
+	}
+	return t
 }
 
-func (ctx *ManufactureParameters) ShowRequirement(reqs []ResourceRate) {
-	resultMap := map[string]float64{}
+func (mp *ManufactureParameters) ShowRequirement(reqs map[string]float64, outputTemplate string) {
 	fmt.Println("====目标====")
-	for _, req := range reqs {
-		m := ctx.calculateRequirement(req)
-		for k, v := range m {
-			resultMap[k] += v
-		}
-		fmt.Printf("%s %.2f/s\n", req.ResourceName, req.Rate)
+	for res, rate := range reqs {
+		fmt.Printf("%s %.3f/s\n", res, rate)
 	}
 
-	fmt.Println("====生产线====")
-	result := make([]*FacilityRequirement, 0)
-	facilitiesCount := map[string]float64{}
-	for resourceName, facilityNum := range resultMap {
-		resource := ctx.getResource(resourceName)
-		fr := &FacilityRequirement{
-			Resource:   resourceName,
-			Facility:   resource.Facility,
-			Num:        facilityNum,
-			OutputRate: resource.Num / resource.Duration * facilityNum,
-		}
-		for _, subRes := range resource.Requirements {
-			fr.Inputs = append(fr.Inputs, ResourceRate{
-				ResourceName: subRes.ResourceName,
-				Rate:         subRes.Num / resource.Duration * facilityNum,
-			})
-		}
-		result = append(result, fr)
-		facilitiesCount[fr.Facility] += facilityNum
+	producesMap := mp.calculateProduce(reqs)
+	produces := make([]*Produce, 0, len(producesMap))
+	for _, p := range producesMap {
+		produces = append(produces, p)
 	}
 
-	sort.Slice(result, func(i, j int) bool {
-		res1 := ctx.getResource(result[i].Resource)
+	sort.Slice(produces, func(i, j int) bool {
+		res1 := produces[i]
 		ro1 := resourceOrder[res1.Key()]
 
-		res2 := ctx.getResource(result[j].Resource)
+		res2 := produces[j]
 		ro2 := resourceOrder[res2.Key()]
 
 		if ro1 != ro2 {
 			return ro1 < ro2
 		}
-		return res1.Name < res2.Name
+		return res1.ResourceName < res2.ResourceName
 	})
-	for _, item := range result {
-		if item.Num != 0 {
-			fmt.Println(ctx.FormatReq(item))
+
+	fmt.Println("====生产线====")
+	templateLine := strings.Split(strings.Trim(outputTemplate, "\n"), "\n")
+	for _, line := range templateLine {
+		if p, ok := producesMap[line]; ok {
+			fmt.Println(mp.FormatReq(p))
+			delete(producesMap, line)
+		} else {
+			fmt.Println(line)
+		}
+	}
+	fmt.Println()
+	for _, p := range produces {
+		if p, ok := producesMap[p.ResourceName]; ok {
+			fmt.Println(mp.FormatReq(p))
 		}
 	}
 
 	fmt.Println("====设施====")
-	for fac, num := range facilitiesCount {
-		if num != 0 {
-			fmt.Printf("%s: %.2f\n", fac, num)
-		}
+	facilitiesCount := map[string]float64{}
+	for _, p := range produces {
+		facilitiesCount[p.Facility] += p.FacilityNum
 	}
+	for fac, num := range facilitiesCount {
+		fmt.Printf("%s: %.3f\n", fac, num)
+	}
+}
+
+func (mp *ManufactureParameters) FormatReq(p *Produce) string {
+	s := fmt.Sprintf("%s %.3f/s <- (%s:%.3f)", p.OutputRes, p.OutputRate, p.Facility, p.FacilityNum)
+	inputNames := make([]string, 0, len(p.Inputs))
+	for inputName := range p.Inputs {
+		inputNames = append(inputNames, inputName)
+	}
+
+	switch p.SprayType {
+	case SpraySpeedUp:
+		s += " <- 加速"
+	case SprayBonus:
+		s += " <- 增产"
+	}
+	if p.SprayType != SprayDisabled {
+		s += fmt.Sprintf("(%s: %.3f/s)", mp.SprayingClass.Resource, p.SprayRate)
+	}
+
+	if len(p.Inputs) > 0 {
+		sort.Slice(inputNames, func(i, j int) bool {
+			order1 := resourceOrder[inputNames[i]]
+			order2 := resourceOrder[inputNames[j]]
+			return order1 < order2
+		})
+		s += " <- ( "
+		for _, inputRes := range inputNames {
+			inputRate := p.Inputs[inputRes]
+			s += fmt.Sprintf("%s: %.2f/s", inputRes, inputRate)
+			s += " "
+		}
+		s += ")"
+	}
+	return s
 }
